6c905ac58205a0837a4fe884fea31051480a0068
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-01 12:01:18 -0300

    fix: remove duplication when reloading the page

--- app_user_registration/models.py
+++ app_user_registration/models.py
@@ -4,3 +4,9 @@ class user(models.Model):
     id_user = models.AutoField(primary_key=True)
     name = models.TextField(max_length=255)
     age = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        if user.objects.filter(name=self.name, age=self.age).exists():
+            pass
+        else:
+            super(user, self).save(*args, **kwargs)
